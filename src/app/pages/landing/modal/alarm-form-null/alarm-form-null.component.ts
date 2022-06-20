e1124e0ccaa1e710e8bd72857b86b8926d0f402b
LAPTOP-ISLRLS5M\aqlik <aqlika05@gmail.com>
2022-06-20 12:30:45 +0700

    20-06-2022

---
+++ src/app/pages/landing/modal/alarm-form-null/alarm-form-null.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alarm-form-null',
+  templateUrl: './alarm-form-null.component.html',
+  styleUrls: ['./alarm-form-null.component.scss'],
+})
+export class AlarmFormNullComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/pages/landing/modal/alarm-unit-tolak/alarm-unit-tolak.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alarm-unit-tolak',
+  templateUrl: './alarm-unit-tolak.component.html',
+  styleUrls: ['./alarm-unit-tolak.component.scss'],
+})
+export class AlarmUnitTolakComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
